901398baf39f86275937888ed23e94d0f1b168f5
Luiz Henrique dos Anjos <lhenriqueanjos@gmail.com>
2018-01-19 23:13:10 -0200

    corrige exportação das rotas

--- app/routes/Routes.js
+++ app/routes/Routes.js
@@ -3,7 +3,9 @@ import { StackNavigator } from 'react-navigation';
 import AboutContainer from '../containers/AboutContainer';
 import HomeContainer from '../containers/HomeContainer';
 
-export default StackNavigator({
+const Routes = StackNavigator({
   Home: { screen: HomeContainer },
   About: { screen: AboutContainer }
 });
+
+export default Routes;
